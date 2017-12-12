497a7eee20d1e763ff87b1c0c95f8d99df327399
JeremyMcCormick <jermccormick@gmail.com>
2017-12-12 14:47:37 -0800

    Add interface for SVT hit efficiency condition.

---
+++ conditions/src/main/java/org/hps/conditions/svt/SvtHitEfficiency.java
@@ -0,0 +1,28 @@
+package org.hps.conditions.svt;
+
+import org.hps.conditions.api.BaseConditionsObject;
+import org.hps.conditions.api.BaseConditionsObjectCollection;
+import org.hps.conditions.database.Field;
+import org.hps.conditions.database.Table;
+
+/**
+ * Condition mapping SVT channels to their calculated hit efficiency.
+ */
+@Table(names = {"svt_hit_efficiencies"})
+class SvtHitEfficiency extends BaseConditionsObject {
+
+    @SuppressWarnings("serial")
+    public static class SvtHitEfficiencyCollection extends BaseConditionsObjectCollection<SvtGain> {
+    }
+
+    @Field(names = {"svt_channel_id"})
+    public Integer getChannelID() {
+        return this.getFieldValue(Integer.class, "svt_channel_id");
+    }
+
+    @Field(names = {"efficiency"})
+    public Double getEfficiency() {
+        return this.getFieldValue(Double.class, "efficiency");
+    }
+
+}
