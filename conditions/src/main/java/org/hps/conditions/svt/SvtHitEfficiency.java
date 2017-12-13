39958d4543f2cd0a276173144061a101d4d746e4
JeremyMcCormick <jermccormick@gmail.com>
2017-12-13 13:42:14 -0800

    Correct table name in test.

--- conditions/src/main/java/org/hps/conditions/svt/SvtHitEfficiency.java
+++ conditions/src/main/java/org/hps/conditions/svt/SvtHitEfficiency.java
@@ -4,11 +4,14 @@ import org.hps.conditions.api.BaseConditionsObject;
 import org.hps.conditions.api.BaseConditionsObjectCollection;
 import org.hps.conditions.database.Field;
 import org.hps.conditions.database.Table;
+import org.hps.conditions.database.Converter;
+import org.hps.conditions.database.MultipleCollectionsAction;
 
 /**
  * Condition mapping SVT channels to their calculated hit efficiency.
  */
 @Table(names = {"svt_hit_efficiencies"})
+@Converter(multipleCollectionsAction = MultipleCollectionsAction.LAST_UPDATED)
 public class SvtHitEfficiency extends BaseConditionsObject {
 
     @SuppressWarnings("serial")
